883e21131ff1f0e79b0c44d28be9a6194739b047
Zia <ziaualhassain@gmail.com>
2023-03-04 19:01:59 +0530

    Added User Login
    The push contains the user forn=m and the doc form with a toggle button not the switch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-
-
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent },
-    ])
-  ],
+  declarations: [AppComponent, HomeComponent, AuthComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
