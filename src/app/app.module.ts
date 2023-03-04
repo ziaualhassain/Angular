d9f3b8850eca7c6af5e54ef006436639861eca13
Zia <ziaualhassain@gmail.com>
2023-03-04 21:33:20 +0530

    Added toggle button
    Completed the ui and ux of the with minimal functions like toggling to the specific user or the doctor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AuthComponent } from './auth/auth.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, AuthComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatIconModule, MatSlideToggleModule],
   providers: [],
   bootstrap: [AppComponent]
 })
