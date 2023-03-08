3d104804215b1f9266ebf1e08ec7ebee35246cbf
Zia <ziaualhassain@gmail.com>
2023-03-08 18:31:59 +0530

    The form fields are mandatory
    UPDATE : Made that all the fields are mandatory while submitting the form for Patients and Doctors if any filed is missed there will be a alert and on successful submission a alert will be displayed!

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -31,27 +31,46 @@ export class AuthComponent implements OnInit {
 
   constructor(private router: Router) {}
 
+//DOCTOR FORM SUBMISSION LOGIC
   onDoctorFormSubmit()
   {
 
-    //Printing the details of Doctors to the console to cross check
-    console.log('Name:', this.doctorName);
+    //To verify if all of the fields are entered or not? cause the fields are needed to verify the doctor background
+    if (!this.doctorName || !this.doctorRegNo || !this.doctorRegYear || !this.doctorSMC)
+    {
+      alert('All fields are mandatory');
+      return;
+    }
+
+    
+    console.log('Name:', this.doctorName); //Printing the details of Doctors to the console to cross check
     console.log('Reg.No:', this.doctorRegNo);
     console.log('Reg.Year:', this.doctorRegYear);
     console.log('SMC:', this.doctorSMC);
 
+   
     //On clicking the submit button
-    console.log('Doctor form submitted');
-    // this.router.navigate(['/doctor']); //redirecting is not need cause we need to mint a NFT.
+    alert('Minting successful! Wait for verification'); // to display the alert minting is successful
+    console.log('Doctor form submitted'); //to display it in console
+    this.router.navigate(['/doctor']); //redirecting is not need cause we need to mint a NFT.
   }
 
-  onPatientFormSubmit(){
 
-    //Printing the Username for the patient to the console to cross check
-    console.log('Name:', this.patientName);
+//PATIENT FORM SUBMISSION LOGIC
+  onPatientFormSubmit()
+  {
+      //Mandatory to enter the username
+      if (!this.patientName) 
+      {
+        alert('All fields are mandatory');
+        return;
+      }
 
+    
+    console.log('Name:', this.patientName); //Printing the Username for the patient to the console to cross check
+    alert('Minting successful!'); //to display an alrert
     console.log('New Patient Minted!')
-   // this.router.navigate(['/patient']); //redirecting is not need cause we need to mint a NFT
+    this.router.navigate(['/patient']); //redirecting is not need cause we need to mint a NFT
   }
 
 
